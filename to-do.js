60a39227ea3d3123c16cb1e946e04323081188fd
Danton de Lima <dantondelima.contato@gmail.com>
2020-01-09 11:40:16 -0300

    Fixes

--- to-do.js
+++ to-do.js
@@ -2,7 +2,36 @@ var btnCriaLista = document.querySelector("#cria-lista");
 var btnCriaTarefa = document.querySelector("#cria-tarefa");
 var container = document.querySelector("#container")
 var select = document.querySelector("#lista-select");
-function criaLista(){
+function criaLista(nomeLista){
+    let nome = document.createTextNode(nomeLista);
+    let titulo = document.createElement("h1");
+    let lista = document.createElement("ul");
+    lista.id = nomeLista.replace(/ /g, "-");
+    let divLista = document.createElement("div");
+    titulo.appendChild(nome);
+    divLista.appendChild(titulo);
+    divLista.appendChild(lista);
+    container.appendChild(divLista);
+    let item = document.createElement("option");
+    item.text = nomeLista;
+    item.value = nomeLista;
+    select.appendChild(item);
+} 
+
+function criaTarefa(listaId){
+    listaId = listaId.replace(/ /g, "-");
+    console.log(listaId);
+    let nomeTarefa = document.querySelector("#nome-tarefa").value;
+    let nome = document.createTextNode(nomeTarefa);
+    let tarefa = document.createElement("li");
+    let divTarefa = document.createElement("div");
+    divTarefa.appendChild(nome);
+    tarefa.appendChild(divTarefa);
+    lista = document.querySelector("#"+ listaId + "");
+    lista.appendChild(tarefa);
+} 
+
+btnCriaLista.onclick = function(){
     let nomeLista = document.querySelector("#nome-lista").value;
     if(nomeLista == ""){
         alert("O nome da lista não pode ser vazio");
@@ -16,45 +45,17 @@ function criaLista(){
             };
         }
     }
-    let nome = document.createTextNode(nomeLista);
-    let titulo = document.createElement("h1");
-    let lista = document.createElement("ul");
-    lista.id = nomeLista;
-    let divLista = document.createElement("div");
-    titulo.appendChild(nome);
-    divLista.appendChild(titulo);
-    divLista.appendChild(lista);
-    container.appendChild(divLista);
-    let item = document.createElement("option");
-    item.text = nomeLista;
-    item.value = nomeLista;
-    select.appendChild(item);
-} 
-
-function criaTarefa(listaId){
-    console.log(listaId);
-    let nomeTarefa = document.querySelector("#nome-tarefa").value;
-    let nome = document.createTextNode(nomeTarefa);
-    let tarefa = document.createElement("li");
-    let divTarefa = document.createElement("div");
-    divTarefa.appendChild(nome);
-    tarefa.appendChild(divTarefa);
-    lista = document.querySelector("#"+ listaId + "");
-    lista.appendChild(tarefa);
-} 
-
-btnCriaLista.onclick = function(){
-    criaLista();
-    saveToStorage();
+    criaLista(nomeLista);
+    // saveToStorage();
 }
 
 btnCriaTarefa.onclick = function(){
     if(select.length < 2){
-        alert("Crie uma lista antes de adicionar uma tarefa!")
+        alert("Crie uma lista antes de adicionar tarefas!")
         return false;
     }
-    else if(select.value == "Selecione uma lista de tarefa"){
-        alert("Selecione uma lista de tarefa!");
+    else if(select.value == "default"){
+        alert("Selecione uma lista de tarefas!");
         return false;
     }
     criaTarefa(select.value);
